ec597f1c0a863b8cb6f2ec41d9ea86a23f911dbf
nrt18 <150307710+nrt18@users.noreply.github.com>
2024-10-20 16:06:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const Enmap = require('enmap')
+
+const app = express()
+app.use(require('cors')())
+app.use(express.static('public'))
+
+const db = new Enmap({
+	name: "db",
+	dataDir: "./db"
+})
+db.ensure("clicks", 0)
+
+app.get("/api/count", (req, res) => {
+	res.json({
+		count: db.get("clicks")
+	})
+})
+
+app.post("/api/count", (req, res) => {
+	db.math("clicks", "+", 1)
+	res.send("+1")
+})
+
+const port = process.env.PORT || 80
+app.listen(port, () => {
+	console.log(`Launced server on port ${port}.`)
+})
